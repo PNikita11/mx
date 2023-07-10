f5d30359796863c91a93ce1df4e5499e6088a03a
PNikita11 <pendamnikita6550@gmail.com>
2023-07-10 13:20:35 +0530

    third commit

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,61 +16,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            SizedBox(
-              height: 250,
-            ),
-            Image(
-              image: AssetImage("assets/images/metabolix splash screen.jpg"),
-              width: 780,
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Text(
-              "..........where science meets fitness",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-                fontFamily: 'MontSerrat', // Set the font to 'MontSerrat'
-              ),
-            ),
-            SizedBox(
-              height: 150,
-            ),
-            SpinKitPulsingGrid(
-              color: Colors.redAccent,
-              size: 75.0,
-              duration: const Duration(milliseconds: 1000),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-//By Nikita
---
+++ lib/pages/splash.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _zoomInAnimation;
+  late Animation<Offset> _flyInAnimation;
+  late Animation<double> _fadeInAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+
+    _zoomInAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: const Interval(0.0, 0.4, curve: Curves.easeIn),
+      ),
+    );
+
+    _flyInAnimation = Tween<Offset>(
+      begin: const Offset(-1, 0),
+      end: const Offset(0, 0),
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: const Interval(0.5, 0.9, curve: Curves.easeOut),
+      ),
+    );
+
+    _fadeInAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: const Interval(0.8, 1.0, curve: Curves.easeIn),
+      ),
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 90,
+            ),
+            SizedBox(
+              height: 90,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SlideTransition(
+                    position: _flyInAnimation,
+                    child: ScaleTransition(
+                      scale: _zoomInAnimation,
+                      child: Image(
+                        image: AssetImage("assets/images/metabolix icon.jpg"),
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  SlideTransition(
+                    position: _flyInAnimation,
+                    child: ScaleTransition(
+                      scale: _zoomInAnimation,
+                      child: Image(
+                        image: AssetImage("assets/images/metabolix text.jpg"),
+                        width: 250,
+                        height: 250,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            FadeTransition(
+              opacity: _fadeInAnimation,
+              child: Text(
+                ".... where science meets fitness",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  fontFamily: 'MontSerrat',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            SlideTransition(
+              position: _flyInAnimation,
+              child: ScaleTransition(
+                scale: _zoomInAnimation,
+                child: LoadingAnimationWidget.discreteCircle(
+                  color: Colors.deepPurpleAccent,
+                  size: 50,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
